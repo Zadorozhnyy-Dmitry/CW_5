f23089038494fea426f79180cd2318ae8e724f2c
Zadorozhnyy Dmitry <zadorozhnyy_d_m@mail.ru>
2024-07-24 16:20:20 +0300

    реализован интерфейс взаимодействия с пользователем

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 HH_URL = 'https://api.hh.ru/vacancies'
 
 ID_EMPLOYERS_LIST = ['80', '67611',
@@ -6,3 +8,8 @@ ID_EMPLOYERS_LIST = ['80', '67611',
                      '3388', '84585',
                      '41862', '3776'
                      ]
+PARAM_FILE_NAME = Path(__file__).parent.joinpath('database.ini')
+
+SECTION = 'postgresql'
+
+DATA_BASE_NAME = 'cw_5'
